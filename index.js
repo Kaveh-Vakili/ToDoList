c3665fa79c87fbfc637290e63dfab406b1c2dc5c
Kaveh Vakili <vakilik7@gmail.com>
2023-09-02 19:05:17 -0400

    renamed const variables

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-window.addEventListener('load', () => {
-	const form = document.querySelector("new-task-form");
-	const input = document.querySelector("new-task-input");
-	const list_el = document.querySelector("tasks");
-
-    form.addEventListener('submit', (e) => {
-		e.preventDefault();
-
-
-
-	});
-});
+const inputBox= document.getElementById("input-box");
+const list=document.getElementById("list");
