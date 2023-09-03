f2e6a63c58f5df91dfdcc696abd50be64209ed3b
Kaveh Vakili <vakilik7@gmail.com>
2023-09-02 20:43:51 -0400

    added add task function

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const inputBox= document.getElementById("input-box");
 const list=document.getElementById("list");
+
+function addTask(){
+	if(inputBox.value==''){
+		alert('Must type Something!');
+	}
+
+	else{
+		let li=document.createElement("li");
+
+		li.innerHTML=inputBox.value;
+		list.appendChild(li);
+
+	}
+
+
+
+
+
+
+}
